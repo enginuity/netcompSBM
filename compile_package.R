9b7ff447ec3ac3222dcc4b89beddf6f7ff1fd3c6
Lawrence Wang <625lawrence@gmail.com>
2014-12-09 22:59:27 -0500

    update package and compilation code

--- compile_package.R
+++ compile_package.R
@@ -1,13 +1,24 @@
 ##@S Contains code to compile the netcomp_sbm package (to be run from R)
 
-## Load up needed libraries
-require(metacode)
-require(stringr)
-require(roxygen2)
 
-## Update documentation, and re-run Roxygen on it
+# Optional Steps ----------------------------------------------------------
+## These only need to be run, if the source code was edited. 
+
+## Load up needed libraries
+require(metacode) ## This is a package I've written to speed up my own coding
+require(stringr)
+
+## Update documentation -- this looks for new parameters & 
 update_fx_documentation(FD = FilesDescription(dirlist = "netcomp_sbm/R/"), fill_emptyparam = FALSE)
+
+
+# Compile Package ---------------------------------------------------------
+## Both these steps need to be run in order to build the package properly
+
+## Generate the documentation -- THIS MUST be run before building packages (since the documentation files are not version-controlled, as the version-controlled version is in the raw source code)
+require(roxygen2)
 roxygenise("netcomp_sbm/", clean = TRUE)
 
 ## Install the package
 system("R CMD INSTALL netcomp_sbm")
+
