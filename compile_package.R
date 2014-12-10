f7afdbee905c8afc63db67b4140a83e8b749dd40
Lawrence Wang <625lawrence@gmail.com>
2014-12-10 01:30:15 -0500

    rename metacode into codeProcessing

--- compile_package.R
+++ compile_package.R
@@ -5,7 +5,7 @@
 ## These only need to be run, if the source code was edited. 
 
 ## Load up needed libraries
-require(metacode) ## This is a package I've written to speed up my own coding
+require(codeProcessing) ## This is a package I've written to speed up my own coding
 require(stringr)
 
 ## Update documentation -- this looks for new parameters & 
